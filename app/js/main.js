f24295c74d75ae05ce4ed0930ed8182d190a2c00
TheSplatt-WEB <b-26_sokol@mail.ru>
2020-10-03 01:37:46 +0800

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -2,16 +2,35 @@ $(function(){
         $('.slider-top-wrapper').slick({
         prevArrow: '<button type="button" class="slick-prev slick-arrow" style="background-image: url(../images/arrow-left.svg);"></button>',
         nextArrow: '<button type="button" class="slick-next slick-arrow" style="background-image: url(../images/arrow-right.svg);"></button>',
-        autoplay: true,
+        // autoplay: true,
         pauseOnFocus: false,
         pauseOnHover: false,
         dots: true,
+        responsive: [
+            {
+              breakpoint: 1050,
+              settings: {
+                prevArrow: false,
+                nextArrow: false,
+              }
+            },
+          ]
     });
     $('.header-link').on('click', function (event) {
         event.preventDefault();
         var id = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({ scrollTop: top }, 800);
+    });
+    $('.header-inner__menu-btn, .header-link').on('click', function(){
+        if (!$('.header-inner__menu-btn, .header-link').hasClass('openDone')) {
+            $('.header-inner__menu-btn, .header-link').addClass('openDone');
+            $('.header-inner__list').slideToggle();
+        }
+        else {
+            $('.header-inner__menu-btn, .header-link').removeClass('openDone');
+            $('.header-inner__list').slideToggle();
+        }
     });
 });
 
