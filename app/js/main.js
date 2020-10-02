6888cca926326252e8aa2855997707c8297bfed0
TheSplatt-WEB <b-26_sokol@mail.ru>
2020-10-03 04:47:51 +0800

    added btn-top and fix menu animate

--- app/js/main.js
+++ app/js/main.js
@@ -23,14 +23,32 @@ $(function(){
         $('body,html').animate({ scrollTop: top }, 800);
     });
     $('.header-inner__menu-btn, .header-link').on('click', function(){
-        if (!$('.header-inner__menu-btn, .header-link').hasClass('openDone')) {
-            $('.header-inner__menu-btn, .header-link').addClass('openDone');
-            $('.header-inner__list').slideToggle();
+        if (!$('.header-link').hasClass('openDone')) {
+            $('.header-link').addClass('openDone');
+            $('.header-inner__list li').css({'left': '-10px', 'right': '-10px'});
         }
         else {
-            $('.header-inner__menu-btn, .header-link').removeClass('openDone');
-            $('.header-inner__list').slideToggle();
+            $('.header-link').removeClass('openDone');
+            $('.header-inner__list li').css({'left': '-100vw', 'right': '100vw'});
         }
+    });
+    $(window).on('resize', function () {
+        if ($(window).width() > 851) {
+            $('.header-inner__list li').removeAttr('style');
+        }
+    });
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 500) {
+            $('.btn-top').fadeIn();
+        } else {
+            $('.btn-top').fadeOut();
+        }
+    });
+    $('.btn-top').on('click', function () {
+        $("body,html").animate({
+            scrollTop: 0
+        }, 800);
+        return false;
     });
 });
 
