bcf8c60fc1703d537072ef24b71af71ea5b961cc
TheSplatt-WEB <b-26_sokol@mail.ru>
2021-01-17 01:14:36 +0800

    fixet bags

--- app/contact.php
+++ app/contact.php
@@ -1,46 +1,58 @@
 <?php
-if (isset ($_POST['email'])) {
+if (isset($_POST['email'])) {
   $to = "strahovanie.38.irkutsk@gmail.com";
   $from = "support@strahovanie38.ru";
-  $subject = "Заявка с сайта ".$_SERVER['HTTP_REFERER'];
-  $message = "\n------------Имя клиента:    ".$_POST['name']."\n----------Email клиента:    ".$_POST['email']."\n-----Телефон клиента:     ".$_POST['phone']."\nНаименование банка:    ".$_POST['bank']."\n--------------------Сумма:    ".$_POST['summ']."\n------------Виды спорта:    ".$_POST['sport']."\n-------------------Диагноз:    ".$_POST['diagnoz']."\n-Страна назаначения:    ".$_POST['countryFF']."\n-Период пребывания:    ".$_POST['dateFF']."\n-----------Комментарий:    ".$_POST['projectFF']."\n\nСтраница отправки: ".$_SERVER['HTTP_REFERER'];
+  $subject = "Заявка с сайта " . $_SERVER['HTTP_REFERER'];
+
+  $c = true;
+  $message = "<table style='width: 100%;'>";
+  foreach ($_POST as $key => $value) {
+    if ($value != "" && $key != "") {
+      $message .= "
+			" . (($c = !$c) ? '<tr>' : '<tr style="background-color: #f8f8f8;">') . "
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+			</tr>
+			";
+    }
+  }
+  $message .= "</table>";
 
   $boundary = md5(date('r', time()));
-  $filesize = '';
+
   $headers = "MIME-Version: 1.0\r\n";
   $headers .= "From: " . $from . "\r\n";
   $headers .= "Reply-To: " . $from . "\r\n";
   $headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
-  $message="
-Content-Type: multipart/mixed; boundary=\"$boundary\"
- 
---$boundary
-Content-Type: text/plain; charset=\"utf-8\"
-Content-Transfer-Encoding: 7bit
- 
-$message";
-    if(is_uploaded_file($_FILES['image']['tmp_name'])) {
-      $attachment = chunk_split(base64_encode(file_get_contents($_FILES['image']['tmp_name'])));
-      $filename = $_FILES['image']['name'];
-      $filetype = $_FILES['image']['type'];
-      $filesize = $_FILES['image']['size'];
-      $message.="
 
---$boundary
-Content-Type: \"$filetype\"; name=\"$filename\"
-Content-Transfer-Encoding: base64
-Content-Disposition: attachment; filename=\"$filename\"
+  $body = "Content-Type: multipart/mixed; boundary=\"$boundary\"\n";
+  $body .= "--$boundary\n";
+  $body .= "Content-Type: text/html; charset=\"utf-8\"\n";
+  $body .= "Content-Transfer-Encoding: quoted-printable\n\n";
+  $body .= "$message\n\n";
 
-$attachment";
+  $filesize = 0;
+  $count = 0;
+  foreach ($_FILES['attach']['name'] as $file) {
+    if (is_uploaded_file($_FILES['attach']['tmp_name'][$count])) {
+      $attachment = chunk_split(base64_encode(file_get_contents($_FILES['attach']['tmp_name'][$count])));
+      $filename = $_FILES['attach']['name'][$count];
+      $filetype = $_FILES['attach']['type'][$count];
+      $filesize += $_FILES['attach']['size'][$count];
+      $body .= "--$boundary\n";
+      $body .= "Content-Type: \"$filetype\"; name=\"$filename\"\n";
+      $body .= "Content-Transfer-Encoding: base64\n";
+      $body .= "Content-Disposition: attachment; filename=\"$filename\"\n\n";
+      $body .= $attachment . "\n";
     }
-   $message.="
---$boundary--";
- 
-  if ($filesize < 10000000) { // проверка на общий размер всех файлов. Многие почтовые сервисы не принимают вложения больше 10 МБ
-    mail($to, $subject, $message, $headers);
-    echo $_POST['name'].', Ваше сообщение отправлено, спасибо!';
+    $count++;
+  }
+  $body .= "--$boundary--\n";
+
+  if ($filesize < 50000000) { // проверка на общий размер всех файлов. Многие почтовые сервисы не принимают вложения больше 10 МБ
+    mail($to, $subject, $body, $headers);
+    echo $_POST['name'] . ', Ваше сообщение отправлено, спасибо!';
   } else {
-    echo 'Извините, письмо не отправлено. Размер всех файлов превышает 10 МБ.';
+    echo 'Извините, письмо не отправлено. Размер всех файлов превышает 50 МБ.';
   }
 }
-?>
--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
-$(function(){
-        $('.slider-top-wrapper').slick({
+$(function () {
+    $('.slider-top-wrapper').slick({
         prevArrow: '<button type="button" class="slick-prev slick-arrow" style="background-image: url(../images/arrow-left.svg);"></button>',
         nextArrow: '<button type="button" class="slick-next slick-arrow" style="background-image: url(../images/arrow-right.svg);"></button>',
         autoplay: true,
@@ -8,37 +8,45 @@ $(function(){
         pauseOnFocus: false,
         pauseOnHover: false,
         dots: true,
-        responsive: [
-            {
-              breakpoint: 1050,
-              settings: {
+        responsive: [{
+            breakpoint: 1050,
+            settings: {
                 prevArrow: false,
                 nextArrow: false,
-              }
-            },
-          ]
+            }
+        }, ]
     });
     $('.header-link, .to-form__link').on('click', function (event) {
         event.preventDefault();
-        var id = $(this).attr('href'),
+        let id = $(this).attr('href'),
             top = $(id).offset().top;
-        $('body,html').animate({ scrollTop: top }, 800);
+        $('body,html').animate({
+            scrollTop: top
+        }, 800);
     });
-    $('.header-inner__menu-btn, .header-link').on('click', function(){
+    $('.header-inner__menu-btn, .header-link').on('click', function () {
         if (!$('.header-link').hasClass('openDone')) {
             $('.header-link').addClass('openDone');
-            $('.header-inner__list li').css({'left': '-15px', 'right': '-15px'});
-        }
-        else {
+            $('.header-inner__list li').css({
+                'left': '-15px',
+                'right': '-15px'
+            });
+        } else {
             $('.header-link').removeClass('openDone');
-            $('.header-inner__list li').css({'left': '-100vw', 'right': '150vw'});
+            $('.header-inner__list li').css({
+                'left': '-100vw',
+                'right': '150vw'
+            });
         }
     });
-    $(document).on('click', function(e){
+    $(document).on('click', function (e) {
         if (!$('.header-inner__menu-btn').is(e.target) && $('.header-inner__menu-btn').has(e.target).length === 0 &&
-        !$('.header-inner__list li').is(e.target) && $('.header-inner__list li').has(e.target).length === 0){
+            !$('.header-inner__list li').is(e.target) && $('.header-inner__list li').has(e.target).length === 0) {
             $('.header-link').removeClass('openDone');
-            $('.header-inner__list li').css({'left': '-100vw', 'right': '150vw'});
+            $('.header-inner__list li').css({
+                'left': '-100vw',
+                'right': '150vw'
+            });
         }
     });
     $(window).on('resize', function () {
@@ -60,8 +68,8 @@ $(function(){
         return false;
     });
     $('body').on('change', '.files__input', function () {
-        var name_file = [];
-        for (var i = 0; i < $(this).get(0).files.length; ++i) {
+        let name_file = [];
+        for (let i = 0; i < $(this).get(0).files.length; ++i) {
             name_file.push($(this).get(0).files[i].name);
         }
         $(this).next().next('span').text(name_file.join(", "));
@@ -72,8 +80,7 @@ $(function(){
             $('.files__item-nodrivers').fadeOut(300).show();
             $('.files__item-nodrivers').removeAttr('style');
             $('.files__input-nodrivers').prop('required', false);
-        }
-        else {
+        } else {
             $('.files__item-nodrivers').removeClass('openDone');
             $('.files__item-nodrivers').fadeIn(300);
             $('.files__input-nodrivers').prop('required', true);
@@ -85,87 +92,124 @@ $(function(){
             $('.files__item-noinspect').fadeOut(300).show();
             $('.files__item-noinspect').removeAttr('style');
             $('.files__input-noinspect').prop('required', false);
-        }
-        else {
+        } else {
             $('.files__item-noinspect').removeClass('openDone');
             $('.files__item-noinspect').fadeIn(300);
             $('.files__input-noinspect').prop('required', true);
         }
     });
-    $('body').on('click', '.remove__driver', function(){
+    $('body').on('click', '.remove__driver', function () {
         $(this).parent().parent().remove()
     });
-    $('body').on('click', '.added__driver', function(){
-        $('.drivers__inner').append('<div class="files__item files__item-nodrivers"><div class="files__name"><span>*</span>Водительское удостоверение<a class="remove__driver" href="javascript:;">(удалить)</a></div><div class="files__buttons"><div class="files__btn"><input class="files__input files__input-nodrivers" name="image" type="file" accept=".jpg, .png, .gif, .jpeg, .pdf" required><div class="files__choose">Лицевая</div><span>файл не выбран</span></div><div class="files__btn"><input class="files__input files__input-nodrivers" name="image" type="file" accept=".jpg, .png, .gif, .jpeg, .pdf" required><div class="files__choose">Оборотная</div><span>файл не выбран</span></div></div></div>')
+    $('body').on('click', '.added__driver', function () {
+        $('.drivers__inner').append('<div class="files__item files__item-nodrivers"><div class="files__name"><span>*</span>Водительское удостоверение<a class="remove__driver" href="javascript:;">(удалить)</a></div><div class="files__buttons"><div class="files__btn"><input class="files__input files__input-nodrivers" name="attach[]" type="file" accept=".jpg, .png, .gif, .jpeg, .pdf" required><div class="files__choose">Лицевая</div><span>файл не выбран</span></div><div class="files__btn"><input class="files__input files__input-nodrivers" name="attach[]" type="file" accept=".jpg, .png, .gif, .jpeg, .pdf" required><div class="files__choose">Оборотная</div><span>файл не выбран</span></div></div></div>')
     });
-    $('body').on('click', '.added__man', function(){
-        $('.drivers__inner').append('<div class="files__item files__item-nodrivers"><div class="files__name"><span>*</span>Паспорт страхуемого лица<a class="remove__driver" href="javascript:;">(удалить)</a></div><div class="files__buttons"><div class="files__btn"><input class="files__input files__input-nodrivers" name="image" type="file" accept=".jpg, .png, .gif, .jpeg, .pdf" required><div class="files__choose">Главная</div><span>файл не выбран</span></div><div class="files__btn"><input class="files__input files__input-nodrivers" name="image" type="file" accept=".jpg, .png, .gif, .jpeg, .pdf" required><div class="files__choose">Прописка</div><span>файл не выбран</span></div></div></div>')
+    $('body').on('click', '.added__man', function () {
+        $('.drivers__inner').append('<div class="files__item files__item-nodrivers"><div class="files__name"><span>*</span>Паспорт страхуемого лица<a class="remove__driver" href="javascript:;">(удалить)</a></div><div class="files__buttons"><div class="files__btn"><input class="files__input files__input-nodrivers" name="attach[]" type="file" accept=".jpg, .png, .gif, .jpeg, .pdf" required><div class="files__choose">Главная</div><span>файл не выбран</span></div><div class="files__btn"><input class="files__input files__input-nodrivers" name="attach[]" type="file" accept=".jpg, .png, .gif, .jpeg, .pdf" required><div class="files__choose">Прописка</div><span>файл не выбран</span></div></div></div>')
     });
-    $('.hint-link__kasko').on('click', function(){
+    $('.hint-link__kasko').on('click', function () {
         $('.hint-link__descr-kasko').fadeToggle();
     });
-    $(document).on('click', function(e){
+    $(document).on('click', function (e) {
         if (!$('.hint-link__kasko').is(e.target) && $('.hint-link__kasko').has(e.target).length === 0 &&
-        !$('.hint-link__descr-kasko').is(e.target) && $('.hint-link__descr-kasko').has(e.target).length === 0){
+            !$('.hint-link__descr-kasko').is(e.target) && $('.hint-link__descr-kasko').has(e.target).length === 0) {
             $('.hint-link__descr-kasko').fadeOut();
         }
     });
-    $('.hint-link__life').on('click', function(){
+    $('.hint-link__life').on('click', function () {
         $('.hint-link__descr-life').fadeToggle();
     });
-    $(document).on('click', function(e){
+    $(document).on('click', function (e) {
         if (!$('.hint-link__life').is(e.target) && $('.hint-link__life').has(e.target).length === 0 &&
-        !$('.hint-link__descr-life').is(e.target) && $('.hint-link__descr-life').has(e.target).length === 0){
+            !$('.hint-link__descr-life').is(e.target) && $('.hint-link__descr-life').has(e.target).length === 0) {
             $('.hint-link__descr-life').fadeOut();
         }
     });
-    $(".fancybox-form").on('submit', function() {
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: th.serialize()
-		}).done(function() {
-			alert("Ваша заявка принята. Наш специалист свяжется с Вами в течении 15 минут.");
-			setTimeout(function() {
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
+    $(".fancybox-form").on('submit', function () {
+        let th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php",
+            data: th.serialize()
+        }).done(function () {
+            alert("Ваша заявка принята, спасибо.");
+            setTimeout(function () {
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
     });
-    $.fn.setCursorPosition = function(pos) {
+    $.fn.setCursorPosition = function (pos) {
         if ($(this).get(0).setSelectionRange) {
-          $(this).get(0).setSelectionRange(pos, pos);
+            $(this).get(0).setSelectionRange(pos, pos);
         } else if ($(this).get(0).createTextRange) {
-          var range = $(this).get(0).createTextRange();
-          range.collapse(true);
-          range.moveEnd('character', pos);
-          range.moveStart('character', pos);
-          range.select();
+            let range = $(this).get(0).createTextRange();
+            range.collapse(true);
+            range.moveEnd('character', pos);
+            range.moveStart('character', pos);
+            range.select();
         }
-      };
-      $('input[type="tel"]').on('click', function(){
+    };
+    $('input[type="tel"]').on('click', function () {
         $(this).setCursorPosition(3);
-      });
+    });
     $("input[type=tel]").mask("+7 (999) 999-9999");
 });
-$(function() {
-    document.getElementById('form').addEventListener('submit', function(evt){
-      var http = new XMLHttpRequest(), f = this;
-      var th = $(this);
-      evt.preventDefault();
-      http.open("POST", "contact.php", true);
-      http.onreadystatechange = function() {
-        if (http.readyState == 4 && http.status == 200) {
-          alert(http.responseText);
-          if (http.responseText.indexOf(f.name.value) == 0) { // очистить поля формы, если в ответе первым словом будет имя отправителя (name)
-            th.trigger("reset");
-          }
-        }
-      }
-      http.onerror = function() {
-        alert('Ошибка, попробуйте еще раз');
-      }
-      http.send(new FormData(f));
+
+$(function () {
+    document.getElementById('form').addEventListener('submit', function (evt) {
+
+        let pr = document.querySelector('.progress-bar'),
+            checkboxNoInspect = document.querySelector('.files__item-noinspect'),
+            NoInspect = document.querySelectorAll('.files__input-noinspect'),
+            checkboxNoDriver = document.querySelectorAll('.files__item-nodrivers'),
+            noDriver = document.querySelectorAll('.files__input-nodrivers'),
+            inputs = document.getElementById('form').querySelectorAll('.files__btn span');
+
+        let http = new XMLHttpRequest(),
+            f = this;
+        let th = $(this);
+        evt.preventDefault();
+
+        http.upload.onloadstart = function () {
+            pr.style.opacity = 1;
+            pr.style.visibility = 'visible';
+        };
+
+        http.onreadystatechange = function () {
+            if (http.readyState == 4 && http.status == 200) {
+                alert(http.responseText);
+                console.log(http.responseText);
+                if (http.responseText.indexOf(f.name.value) == 0) { // очистить поля формы, если в ответе первым словом будет имя отправителя (name)
+                    th.trigger("reset");
+                    inputs.forEach(element => {
+                        element.textContent = 'файл не выбран';
+                    });
+                    checkboxNoInspect.classList.remove('openDone');
+                    checkboxNoInspect.style.display = '';
+                    checkboxNoDriver.forEach(element => {
+                        element.classList.remove('openDone');
+                    });
+                    checkboxNoDriver.forEach(element => {
+                        element.style = '';
+                    });
+                    NoInspect.forEach(element => {
+                        element.setAttribute('required', true);
+                    });
+                    noDriver.forEach(element => {
+                        element.setAttribute('required', true);
+                    });
+                    pr.style.opacity = 0;
+                    pr.style.visibility = 'hidden';
+                }
+            }
+        };
+        http.onerror = function () {
+            alert('Ошибка, попробуйте еще раз');
+        };
+
+        http.open("POST", "contact.php", true);
+        http.send(new FormData(f));
+
     }, false);
 });
